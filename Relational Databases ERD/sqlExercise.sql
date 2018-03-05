2281de86654f8470e636c4e5881ab76069022024
Tim A. Conner <timaconner1@gmail.com>
2018-03-05 14:57:43 -0600

    Add Relational Database Exercise Part #1.

---
+++ Relational Databases ERD/sqlExercise.sql
@@ -0,0 +1,5 @@
+
+--For each of the following exercises, provide the appropriate query. Everything from class and the [Sqlite](http://www.sqlite.org/) documentation for SQL [keywords](https://www.sqlite.org/lang.html) and [functions](https://www.sqlite.org/lang_corefunc.html) is fair game. Note that 
+
+--1. Query all of the entries in the `Genre` table
+select * from Genre;
