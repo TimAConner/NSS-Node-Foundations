245ea12b2474d994294893635ce7254f3b21fb06
Tim A. Conner <timaconner1@gmail.com>
2018-03-05 14:58:21 -0600

    Add Relational Database Exercise Part #5.

--- Relational Databases ERD/sqlExercise.sql
+++ Relational Databases ERD/sqlExercise.sql
@@ -15,3 +15,9 @@ INSERT INTO Song ("Title", "SongLength", "ReleaseDate", "GenreId", "ArtistId", "
  INSERT INTO Song ("Title", "SongLength", "ReleaseDate", "GenreId", "ArtistId", "AlbumId")
  VALUES ("DARE", "4:04", "29 Aug 2005", (select GenreId from Genre where Label = "Rock"), (select ArtistId from Artist where ArtistName = "Gorillaz"), (select AlbumId from Album where Title = "Demon Days"));
  
+--1. Write a `SELECT` query that provides the song titles, album title, and artist name for all of the data you just entered in. Use the [`LEFT JOIN`](https://www.tutorialspoint.com/sql/sql-using-joins.htm) keyword sequence to connect the tables, and the `WHERE` keyword to filter the results to the album and artist you added. Here is some more info on [joins](http://www.dofactory.com/sql/join) that might help.
+SELECT s.title "Song Title", a.title "Album Title", b.artistName
+FROM Song AS s 
+LEFT JOIN Album AS a ON s.albumId = a.albumId 
+LEFT JOIN Artist as b ON s.artistId = b.artistId
+WHERE s.AlbumId = (SELECT AlbumId FROM Album WHERE title = "Demon Days");
