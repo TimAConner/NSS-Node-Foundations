6881544e8caaaaef71140f8bba5c37a50b2ae50c
Tim A. Conner <timaconner1@gmail.com>
2018-03-05 17:09:44 -0600

    Update SQLite Create DB exercise.

--- SQLite Create DB/SQLite Create DB.js
+++ SQLite Create DB/SQLite Create DB.js
@@ -3,9 +3,12 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database("company.sqlite");
 const createTable = require('./createTable');
 
-const sqlPromise = (sqlFunction, sqlCode, printIndividualLines) => {
+const sqlPromise = ({sqlFunction, sqlCode, printIndividualLines = false, comment = undefined}) => {
     return new Promise((resolve, reject) => {
         db[sqlFunction](sqlCode, (err, data) => {
+            if(comment){
+                console.log(comment);
+            }
             if(printIndividualLines){
                 for(let sqlRow of data){
                     for(let prop in sqlRow){
@@ -20,16 +23,46 @@ const sqlPromise = (sqlFunction, sqlCode, printIndividualLines) => {
 };
 
 createTable().then(() => {
-    sqlPromise('run', `INSERT INTO employees VALUES ('01', 'Tim', 'Conner', 'The Indubitable Man', '247 My House Lane');`)
-    .then(() => sqlPromise('run', `INSERT INTO employees VALUES ('02', 'Joe', 'Conner', 'The Indubitable Fly', '247 My House Lane');`))
-    .then(() => sqlPromise('run', `INSERT INTO employees VALUES ('03', 'Sarah', 'Conner', 'The Fly', '247 My House Lane');`))
-    .then(() => sqlPromise('run', `INSERT INTO employees VALUES ('04', 'Kylie', 'Conner', 'The Indubitable Rock', '247 My House Lane');`))
-    .then(() => sqlPromise('run', `INSERT INTO employees VALUES ('05', 'Blue', 'Conner', 'The Indubitable Boot', '247 My House Lane');`))
-    .then(() => sqlPromise('run', `INSERT INTO employees VALUES ('06', 'Dorothy', 'Conner', 'The Indubitable', '247 My House Lane');`))
-    .then(() => sqlPromise('all', `SELECT * FROM employees`))
-    .then(data => console.log('Write a statement to query the database and console.log() all employee records.', data))
-    .then(() => sqlPromise('all', `SELECT jobTitle FROM employees`, true))
-    .then(() => sqlPromise('all', `SELECT firstName, lastName, address FROM employees`, true))
+    sqlPromise({
+        sqlFunction: 'run', 
+        sqlCode: `INSERT INTO employees VALUES ('01', 'Tim', 'Conner', 'The Indubitable Man', '247 My House Lane');`
+    }).then(() => sqlPromise({
+        sqlFunction: 'run', 
+        sqlCode:  `INSERT INTO employees VALUES ('02', 'Joe', 'Conner', 'The Indubitable Fly', '247 My House Lane');`
+    }))
+    .then(() => sqlPromise({
+        sqlFunction: 'run', 
+        sqlCode:  `INSERT INTO employees VALUES ('03', 'Sarah', 'Conner', 'The Fly', '247 My House Lane');`
+    }))
+    .then(() => sqlPromise({
+        sqlFunction: 'run', 
+        sqlCode:  `INSERT INTO employees VALUES ('04', 'Kylie', 'Conner', 'The Indubitable Rock', '247 My House Lane');`
+    }))
+    .then(() => sqlPromise({
+        sqlFunction: 'run', 
+        sqlCode:  `INSERT INTO employees VALUES ('05', 'Blue', 'Conner', 'The Indubitable Boot', '247 My House Lane');`
+    }))
+    .then(() => sqlPromise({
+        sqlFunction: 'run', 
+        sqlCode:  `INSERT INTO employees VALUES ('06', 'Dorothy', 'Conner', 'The Indubitable', '247 My House Lane');`
+    }))
+    .then(() => sqlPromise({
+        sqlFunction: 'all', 
+        sqlCode:  `SELECT * FROM employees`,
+        comment: "-- Write a statement to query the database and console.log() all employee records."
+    }))
+    .then(() => sqlPromise({
+        sqlFunction: 'all', 
+        sqlCode:  `SELECT jobTitle FROM employees`,
+        printIndividualLines: true,
+        comment: "-- Write a statement to query the database and console.log() each employees jobTitle."
+    }))
+    .then(() => sqlPromise({
+        sqlFunction: 'all', 
+        sqlCode:  `SELECT firstName, lastName, address FROM employees`,
+        printIndividualLines: true,
+        comment: "-- Write a statement to query the database and console.log() each employees firstName, lastName and address only."
+    }))
     .catch(err => console.log('err', err));
 });
 
