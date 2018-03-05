7d25b18f417d6452b4dc09036f0c522d9d2d375f
Tim A. Conner <timaconner1@gmail.com>
2018-03-05 14:58:11 -0600

    Add Relational Database Exercise Part #4.

--- Relational Databases ERD/sqlExercise.sql
+++ Relational Databases ERD/sqlExercise.sql
@@ -9,3 +9,9 @@ VALUES ("Gorillaz", "1998");
 --1. Using the `INSERT` statement, add one, or more, albums by your artist to the `Album` table.
 INSERT INTO "Album" ("Title", "ReleaseDate", "AlbumLength", "Label", "ArtistId", "GenreId")
  VALUES ("Demon Days", "2005", "50:59", "Parlophone", (select ArtistId from Artist where ArtistName  = "Gorillaz"),  (select GenreId from Genre where Label = "Rock"));
+--1. Using the `INSERT` statement, add some songs that are on that album to the `Song` table.
+INSERT INTO Song ("Title", "SongLength", "ReleaseDate", "GenreId", "ArtistId", "AlbumId")
+ VALUES ("Feel Good Inc.", "3:41", "9 May 2005", (select GenreId from Genre where Label = "Rock"), (select ArtistId from Artist where ArtistName = "Gorillaz"), (select AlbumId from Album where Title = "Demon Days"));
+ INSERT INTO Song ("Title", "SongLength", "ReleaseDate", "GenreId", "ArtistId", "AlbumId")
+ VALUES ("DARE", "4:04", "29 Aug 2005", (select GenreId from Genre where Label = "Rock"), (select ArtistId from Artist where ArtistName = "Gorillaz"), (select AlbumId from Album where Title = "Demon Days"));
+ 
