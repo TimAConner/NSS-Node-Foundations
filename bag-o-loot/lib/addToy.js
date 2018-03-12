fb8a985dd869b76d9234e74ea9036fb998b4448d
Tim A. Conner <timaconner1@gmail.com>
2018-03-12 14:38:49 -0500

    Refactor addToy to also add a child for bag-o-loot.

--- bag-o-loot/lib/addToy.js
+++ bag-o-loot/lib/addToy.js
@@ -3,8 +3,25 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('bagoloot.sqlite');
 
 module.exports.addToy = ({toyName, childName}) => new Promise((resolve, reject) => {
-    db.run(`INSERT INTO toys VALUES (null, '${toyName}', (SELECT child_id FROM children WHERE name = '${childName}'), 'false')`, function(err, data){
-        if(err) reject(err);
-        else resolve(this.lastID);
+    new Promise((resolve, reject) => {
+        db.get(`SELECT child_id FROM children WHERE name = '${childName}'`, (err, data) => {
+            if (err) reject(err);
+            // The child is currently present
+            if (data) {
+                db.run(`INSERT INTO toys VALUES (null, '${toyName}', (SELECT child_id FROM children WHERE name = '${childName}'), 'false')`, function(err, data){
+                    if (err) reject(err);
+                    else resolve(this.lastID);
+                });
+            } else {
+                // The child is not present, create it.
+                db.run(`INSERT INTO "children" ("child_id", "name")
+                VALUES (null, '${childName}')`, (err, data) => {
+                    db.run(`INSERT INTO toys VALUES (null, '${toyName}', (SELECT child_id FROM children WHERE name = '${childName}'), 'false')`, function(err, data){
+                        if (err) reject(err);
+                        else resolve(this.lastID);
+                    });
+                });
+            }
+        });
     });
 });
